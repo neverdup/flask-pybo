4001e83642688ed83c81044aa761c02d7bf7e111
Jaewon Yang <neverdup@gmail.com>
2021-06-11 21:38:47 +0900

    config 변경

---
+++ config/development.py
@@ -0,0 +1,5 @@
+from config.defalult import *
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'dev'
--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.defalult import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xdd\x01\x1c\x1c\xb0V\x0c\x10\xc0>M\xa1+\xd64\x9e'
